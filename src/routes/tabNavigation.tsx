c8aec2b7a5f21348d4a335724d632f8d416a4313
SofiyaPetryshak <wonderpeace2@gmail.com>
2021-01-22 19:07:53 +0200

    feat: flatlist cards added to wishlist

--- src/routes/tabNavigation.tsx
+++ src/routes/tabNavigation.tsx
@@ -118,7 +118,6 @@ export default function BottomNavigation () {
 
           return <Ionicons name={iconName} size={size} color={color} />
         },
-
       })}
       tabBarOptions={{
         activeTintColor: 'tomato',
