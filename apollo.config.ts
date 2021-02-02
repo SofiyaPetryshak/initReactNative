d6a7f5e7b80eb951b4cf5bb5d144c6f2642de72b
SofiyaPetryshak <wonderpeace2@gmail.com>
2021-02-02 19:20:27 +0200

    error: doesn't work

---
+++ apollo.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'api',
+      url: 'https://api.spacex.land/graphql/',
+    },
+  },
+}
--- src/screens/myAccount.tsx
+++ src/screens/myAccount.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { gql, useQuery } from '@apollo/client'
 
 import { globalStyles } from '../styles/global'
 
+interface SHIP {
+  name: string
+  // eslint-disable-next-line camelcase
+  home_port: string
+  image: string
+  id: string
+
+}
+const GET_SHIPS = gql`
+query GetShips {
+  ships {
+    name
+    home_port
+    image
+    id
+  }
+    }
+    `
 export default function MyAccountScreen () {
+  const { loading, data } = useQuery(GET_SHIPS)
+
   return (
     <View style={globalStyles.container}>
-      <Text>My Account Screen</Text>
+      <Text>Search Screen</Text>
+      <View> {
+        data.ships.map((ship:SHIP) => (
+          <Text key={ship.id}>{ship.home_port}</Text>
+        ))
+      }
+      </View>
     </View>
   )
 }
